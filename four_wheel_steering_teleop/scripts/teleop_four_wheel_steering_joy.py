08d53241371d6f6e4acfb8f119cc76517ef9bdb5
Vincent Rousseau <vincent.rousseau@irstea.fr>
2017-07-06 11:17:41 +0200

    [teleop] Add enable_button

--- four_wheel_steering_teleop/scripts/teleop_four_wheel_steering_joy.py
+++ four_wheel_steering_teleop/scripts/teleop_four_wheel_steering_joy.py
@@ -14,6 +14,9 @@ class TeleopFourWheelSteeringJoy():
         self.axis_rear_steering = 3   # Left left-right stick
         self.scale_steering = pi/10.0
 
+        self.enable_button = [4,5]
+        self.sent_disable_msg = False
+
         self.is_trigger_forward_init = False
         self.is_trigger_reverse_init = False
         self.last_forward_speed = 0.0
@@ -27,40 +30,51 @@ class TeleopFourWheelSteeringJoy():
     def callback(self, data):
         four_wheel_steering_msg = FourWheelSteering()
 
-        #rospy.loginfo(rospy.get_caller_id() + " axes" + str(data.axes))
-        linear_forward = 1.0
-        linear_reverse = 1.0
-        if self.is_trigger_forward_init:
-            linear_forward = data.axes[self.axis_linear_forward]
-        elif abs(self.last_forward_speed - data.axes[self.axis_linear_forward]) > 0.01:
-            self.is_trigger_forward_init =  True
+        new_cmd = False
+        for button in self.enable_button:
+          if data.buttons[button]:
+            new_cmd = True
+
+        if new_cmd:
+            #rospy.loginfo(rospy.get_caller_id() + " axes" + str(data.axes))
+            linear_forward = 1.0
+            linear_reverse = 1.0
+            if self.is_trigger_forward_init:
+                linear_forward = data.axes[self.axis_linear_forward]
+            elif abs(self.last_forward_speed - data.axes[self.axis_linear_forward]) > 0.01:
+                self.is_trigger_forward_init =  True
+            else:
+                self.last_forward_speed = data.axes[self.axis_linear_forward]
+
+            if self.is_trigger_reverse_init:
+                linear_reverse = data.axes[self.axis_linear_reverse]
+            elif abs(self.last_reverse_speed - data.axes[self.axis_linear_reverse]) > 0.01:
+                self.is_trigger_reverse_init =  True
+            else:
+                self.last_reverse_speed = data.axes[self.axis_linear_reverse]
+
+            speed = (-linear_forward + linear_reverse)/2.0
+            #rospy.loginfo(rospy.get_caller_id() + " speed %s", speed)
+
+            if abs(speed) > self.axis_dead_zone:
+                four_wheel_steering_msg.speed = speed*self.scale_linear
+
+            if abs(data.axes[self.axis_front_steering]) > self.axis_dead_zone:
+                four_wheel_steering_msg.front_steering_angle = data.axes[self.axis_front_steering]*self.scale_steering
+            else:
+                four_wheel_steering_msg.front_steering_angle = 0.0
+
+            if abs(data.axes[self.axis_rear_steering]) > self.axis_dead_zone:
+                four_wheel_steering_msg.rear_steering_angle = data.axes[self.axis_rear_steering]*self.scale_steering
+            else:
+                four_wheel_steering_msg.rear_steering_angle = 0.0
+
+            self.pub.publish(four_wheel_steering_msg)
+            self.sent_disable_msg = False
         else:
-            self.last_forward_speed = data.axes[self.axis_linear_forward]
-
-        if self.is_trigger_reverse_init:
-            linear_reverse = data.axes[self.axis_linear_reverse]
-        elif abs(self.last_reverse_speed - data.axes[self.axis_linear_reverse]) > 0.01:
-            self.is_trigger_reverse_init =  True
-        else:
-            self.last_reverse_speed = data.axes[self.axis_linear_reverse]
-
-        speed = (-linear_forward + linear_reverse)/2.0
-        #rospy.loginfo(rospy.get_caller_id() + " speed %s", speed)
-
-        if abs(speed) > self.axis_dead_zone:
-            four_wheel_steering_msg.speed = speed*self.scale_linear
-
-        if abs(data.axes[self.axis_front_steering]) > self.axis_dead_zone:
-            four_wheel_steering_msg.front_steering_angle = data.axes[self.axis_front_steering]*self.scale_steering
-        else:
-            four_wheel_steering_msg.front_steering_angle = 0.0
-
-        if abs(data.axes[self.axis_rear_steering]) > self.axis_dead_zone:
-            four_wheel_steering_msg.rear_steering_angle = data.axes[self.axis_rear_steering]*self.scale_steering
-        else:
-            four_wheel_steering_msg.rear_steering_angle = 0.0
-
-        self.pub.publish(four_wheel_steering_msg)
+            if self.sent_disable_msg == False:
+                self.pub.publish(four_wheel_steering_msg)
+                self.sent_disable_msg = True
 
 
 if __name__ == '__main__':
